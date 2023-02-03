82e072c40542fba752453962893130b9e33c4410
Juche <azhucheng1@qq.com>
2023-02-03 16:41:17 +0800

    update app server file

--- code/node/nodebeginner-app/server.js
+++ code/node/nodebeginner-app/server.js
@@ -1,13 +1,18 @@
 import { createServer } from 'node:http';
+import { parse } from 'node:url';
 
 function start() {
   const hostname = '127.0.0.1';
   const port = '8888';
 
   const server = createServer((req, res) => {
+    const { pathname } = parse(req.url);
+    console.log(`🚀 ~ server ~ request from '${pathname}'`);
     res.statusCode = 200;
     res.setHeader('Content-type', 'text/plain');
-    res.end('Hello Nodejs!');
+    res.write(`Hello ${pathname}`);
+    res.end();
+    debugger;
   });
 
   server.listen(port, hostname, () => {
