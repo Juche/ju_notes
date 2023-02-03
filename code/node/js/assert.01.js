b112b34484a34ca29e821aa43f549093fccd00d6
Juche <azhucheng1@qq.com>
2023-02-03 11:50:41 +0800

    update node assert demo

--- code/node/js/assert.01.js
+++ code/node/js/assert.01.js
@@ -1,7 +1,14 @@
 // import {strict as assert} from 'node:assert';
-import assert from 'node:assert/strict';
+// import assert from 'node:assert/strict';
+// import { equal, deepEqual, notEqual, notDeepEqual } from 'node:assert/strict';
+import { equal, deepEqual, notEqual, notDeepEqual } from 'node:assert';
 
 const arr1 = [[[1, 2, 3]], 4, 5];
 const arr2 = [[[1, 2, '3']], 4, 5];
 
-assert.deepEqual(arr1, arr2);
+const err = new Error('判断错误!');
+
+// equal(arr1, arr2, err);
+// deepEqual(arr1, arr2, err);
+notEqual(arr1, arr2, err);
+// notDeepEqual(arr1, arr2, err);
