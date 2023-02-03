964e75ef1dcda2f83389bfdc96213d0394ed79e8
Juche <azhucheng1@qq.com>
2023-02-03 12:09:56 +0800

    断言错误消息处理

--- code/node/js/assert.01.js
+++ code/node/js/assert.01.js
@@ -1,14 +1,24 @@
 // import {strict as assert} from 'node:assert';
 // import assert from 'node:assert/strict';
-// import { equal, deepEqual, notEqual, notDeepEqual } from 'node:assert/strict';
-import { equal, deepEqual, notEqual, notDeepEqual } from 'node:assert';
+// import { equal, deepEqual, notEqual, notDeepEqual, AssertionError } from 'node:assert/strict';
+import { equal, deepEqual, notEqual, notDeepEqual, AssertionError } from 'node:assert';
 
 const arr1 = [[[1, 2, 3]], 4, 5];
 const arr2 = [[[1, 2, '3']], 4, 5];
 
-const err = new Error('判断错误!');
+const err = (op) => {
+  // return new Error(`[${op}] 判断错误!`);
 
-// equal(arr1, arr2, err);
-// deepEqual(arr1, arr2, err);
-notEqual(arr1, arr2, err);
-// notDeepEqual(arr1, arr2, err);
+  return new AssertionError({
+    message: `[${op}] 判断错误!`,
+    // actual: arr1,
+    // expected: arr2,
+    // operator: op,
+  }).message;
+};
+
+// equal(arr1, arr2);
+equal(arr1, arr2, err('equal'));
+deepEqual(arr1, arr2, err('deepEqual'));
+notEqual(arr1, arr2, err('notEqual'));
+notDeepEqual(arr1, arr2, err('notDeepEqual'));
