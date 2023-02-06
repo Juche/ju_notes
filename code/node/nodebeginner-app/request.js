fbe4984651336f4d34c1722f783350920a5c652f
Juche <azhucheng1@qq.com>
2023-02-06 09:59:41 +0800

    app start 响应返回表单内容

--- code/node/nodebeginner-app/request.js
+++ code/node/nodebeginner-app/request.js
@@ -4,12 +4,34 @@ function start(res) {
   console.log("Request handler 'start' was called.");
   // let ret = 'Home Page!';
   // return ret;
-  exec('dir', (err, stdout, stderr) => {
-    res.statusCode = 200;
-    res.setHeader('Content-type', 'text/plain');
-    res.write(stdout);
-    res.end();
-  });
+  // dir 命令行指令
+  // exec('dir', { timeout: 10000, encoding: 'gbk' }, (err, stdout, stderr) => {
+  //   res.statusCode = 200;
+  //   res.setHeader('Content-type', 'text/plain');
+  //   res.write(stdout);
+  //   res.end();
+  // });
+
+  const body = `
+    <html>
+      <head>
+        <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+      </head>
+      <body>
+        <form action="/upload" method="post">
+          <textarea name="text" rows="20" cols="60"></textarea>
+          <br />
+          <br />
+          <input type="submit" value="Submit text" />
+        </form>
+      </body>
+    </html>
+    `;
+
+  res.statusCode = 200;
+  res.setHeader('Content-type', 'text/html');
+  res.write(body);
+  res.end();
 }
 
 function upload(res) {
