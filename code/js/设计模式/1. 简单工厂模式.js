b413858fcbd78313e079e7f218d5f9676d36aa03
Juche <azhucheng1@qq.com>
2023-02-17 17:46:21 +0800

    设计模式样例更新

--- code/js/设计模式/1. 简单工厂模式.js
+++ code/js/设计模式/1. 简单工厂模式.js
@@ -1,3 +1,13 @@
 /**
  * @简单工厂模式
  */
+
+// function Production(product, price) {
+//   this.product = product;
+//   this.price = price;
+//   console.log(`🚀 ~ 生产的产品: `, product);
+//   console.log(`🚀 ~ 产品的价格: `, price);
+// }
+
+// const phone = new Production('手机', 5000);
+// const laptop = new Production('笔记本电脑', 8000);
--- code/js/设计模式/10. 装饰器模式.js
+++ code/js/设计模式/10. 装饰器模式.js
@@ -1,3 +1,18 @@
 /**
  * @装饰器模式
+ * 不影响代码原有功能的基础上添加新功能
+ * 常用于对老旧代码做功能拓展
  */
+
+let house = () => {
+  console.log('毛坯房');
+};
+
+const _house = house;
+
+house = () => {
+  _house();
+  console.log('精装修');
+};
+
+house();
