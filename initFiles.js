ea9cfb47bbe457985b55740dd70b578d4811ac40
Juche <azhucheng1@qq.com>
2023-02-17 16:58:50 +0800

    文件批量生成脚本优化

--- initFiles.js
+++ initFiles.js
@@ -11,39 +11,47 @@ import { mkdir, readFile } from 'node:fs/promises';
  * [x] initCtt 初始化内容
  */
 
+// TODO: 添加路径感知(tab补全路径)
 stdout.write(`\x1b[33m请输入批量创建文件的配置文件(目录+完整文件名): \x1b[32;4;1m\t`);
 stdin.resume();
 stdin.setEncoding('utf-8');
 stdin.on('data', async (data) => {
   stdin.pause();
-  console.log('\x1b[33;24m输入的内容是：\x1b[102;1m' + data.trim() + '\x1b[0m');
+  // console.log('\x1b[33;24m输入的内容是：\x1b[102;1m' + data.trim() + '\x1b[0m');
 
-  const file = data.trim();
+  const dataTrim = data.trim();
+  const jsonFile = /\.json$/.test(dataTrim) ? dataTrim : `${dataTrim}.json`;
+  const jsonCtt = await parseJsonFile(jsonFile);
+  jsonCtt && createFiles(jsonCtt);
+});
+
+async function parseJsonFile(jsonFile) {
   try {
-    const ctt = await readFile(file, { encoding: 'utf8', withFileTypes: true });
-    const { filePath, fileList, fileOrder, fileType, initCtt } = JSON.parse(ctt);
-    // console.log(`🚀 ~ stdin.on ~ filePath`, filePath);
-    // console.log(`🚀 ~ stdin.on ~ fileList`, fileList);
-    // console.log(`🚀 ~ stdin.on ~ fileOrder`, fileOrder);
-    // console.log(`🚀 ~ stdin.on ~ fileType`, fileType);
-    // console.log(`🚀 ~ stdin.on ~ initCtt`, initCtt);
+    const fileText = await readFile(jsonFile, { encoding: 'utf8', withFileTypes: true });
+    return JSON.parse(fileText);
+  } catch (err) {
+    console.log(`🚀 ~ stdin.on ~ err`, err);
+  }
+}
 
-    try {
-      const path = await mkdir(filePath, { recursive: true });
-      const msg = path ? `创建文件夹 ${path}` : '目标文件夹已存在';
-      console.log(msg);
-    } catch (err) {
-      console.error(err.message);
-    }
+async function createFiles(jsonCtt) {
+  const { filePath, fileList, fileOrder, fileType, initCtt } = jsonCtt;
+  // console.log(`🚀 ~ stdin.on ~ filePath`, filePath);
+  // console.log(`🚀 ~ stdin.on ~ fileList`, fileList);
+  // console.log(`🚀 ~ stdin.on ~ fileOrder`, fileOrder);
+  // console.log(`🚀 ~ stdin.on ~ fileType`, fileType);
+  // console.log(`🚀 ~ stdin.on ~ initCtt`, initCtt);
+
+  try {
+    const path = await mkdir(filePath, { recursive: true });
+    const msg = path ? `创建文件夹 ${path}` : '目标文件夹已存在';
+    console.log(`\x1b[102;1m${msg}\x1b[0m`);
 
     for (const key in fileList) {
       const prefix = fileOrder ? `${Number(key) + 1}. ` : '';
       const name = fileList[key];
       const fileName = `${prefix}${name}.${fileType}`;
       const comment = initCtt.replace('$NAME', name);
-
-      // console.log(`🚀 ~ stdin.on ~ fileName`, fileName);
-      // console.log(`🚀 ~ stdin.on ~ comment`, comment);
 
       appendFile(`${filePath}/${fileName}`, comment, (err) => {
         err && console.log(`🚀 ~ appendFile ~ err`, err);
@@ -52,6 +60,6 @@ stdin.on('data', async (data) => {
       // writeFile(fileName, comment, (err) => {}); // 无则创建,有则覆盖
     }
   } catch (err) {
-    console.log(`🚀 ~ stdin.on ~ err`, err);
+    console.error(err.message);
   }
-});
+}
